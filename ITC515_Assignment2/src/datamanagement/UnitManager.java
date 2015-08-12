dcefae37a7fa0e9cf731291a428eaec34e75e073
Jaarknstein <jaarknstein@users.noreply.github.com>
2015-08-13 02:10:29 +1000

    Committing UnitManager class.
    Commit includes changes to class names, variables and formatting.
    
    Changes have been noted in a change-log.txt file which will contain all
    changes to allocated classes, and will be distributed amongst team.

--- ITC515_Assignment2/src/datamanagement/UnitManager.java
+++ ITC515_Assignment2/src/datamanagement/UnitManager.java
@@ -7,51 +7,46 @@ public class UnitManager {
 
 	private static UnitManager self = null;
 
-	private UnitMap UM;
+	private UnitMap unitMap;
 
-	public static UnitManager UM() {
+	public static UnitManager unitManager() {
 		if (self == null)
 			self = new UnitManager();
 		return self;
 	}
 
 	private UnitManager() {
-		UM = new UnitMap();
+		unitMap = new UnitMap();
 	}
 
-	public IUnit getUnit(String uc) {
-		IUnit iu = UM.get(uc);
-		return iu != null ? iu : createUnit(uc);
+	public InterfaceUnit getUnit(String uc) {
+		InterfaceUnit interfaceUnit = unitMap.get(uc);
+		return interfaceUnit != null ? interfaceUnit : createUnit(uc);
 
 	}
 
-	private IUnit createUnit(String unitCode) {
+	private InterfaceUnit createUnit(String unitCode) {
 
-		IUnit iu;
+		InterfaceUnit interfaceUnit;
 
-		for (Element el : (List<Element>) XMLManager.getXML().getDocument()
-				.getRootElement().getChild("unitTable").getChildren("unit"))
-			if (unitCode.equals(el.getAttributeValue("uid"))) {
-				StudentUnitRecordList slist;
+		for (Element element : (List<Element>) XMLManager.getXML().getDocument().getRootElement().getChild("unitTable")
+				.getChildren("unit"))
+			if (unitCode.equals(element.getAttributeValue("unitId"))) {
+				StudentUnitRecordList studentList;
 
-				slist = null;
-				iu = new Unit(el.getAttributeValue("uid"),
-						el.getAttributeValue("name"), Float.valueOf(
-								el.getAttributeValue("ps")).floatValue(), Float
-								.valueOf(el.getAttributeValue("cr"))
-								.floatValue(), Float.valueOf(
-								el.getAttributeValue("di")).floatValue(), Float
-								.valueOf(el.getAttributeValue("hd"))
-								.floatValue(), Float.valueOf(
-								el.getAttributeValue("ae")).floatValue(),
-						Integer.valueOf(el.getAttributeValue("asg1wgt"))
-								.intValue(), Integer.valueOf(
-								el.getAttributeValue("asg2wgt")).intValue(),
-						Integer.valueOf(el.getAttributeValue("examwgt"))
-								.intValue(), StudentUnitRecordManager
-								.instance().getRecordsByUnit(unitCode));
-				UM.put(iu.getUnitCode(), iu);
-				return iu;
+				studentList = null;
+				interfaceUnit = new Unit(element.getAttributeValue("unitId"), element.getAttributeValue("name"),
+						Float.valueOf(element.getAttributeValue("ps")).floatValue(),
+						Float.valueOf(element.getAttributeValue("cr")).floatValue(),
+						Float.valueOf(element.getAttributeValue("di")).floatValue(),
+						Float.valueOf(element.getAttributeValue("hd")).floatValue(),
+						Float.valueOf(element.getAttributeValue("ae")).floatValue(),
+						Integer.valueOf(element.getAttributeValue("assignment1Weight")).intValue(),
+						Integer.valueOf(element.getAttributeValue("assignment2Weight")).intValue(),
+						Integer.valueOf(element.getAttributeValue("examWeight")).intValue(),
+						StudentUnitRecordManager.instance().getRecordsByUnit(unitCode));
+				unitMap.put(interfaceUnit.getUnitCode(), interfaceUnit);
+				return interfaceUnit;
 			}
 
 		throw new RuntimeException("DBMD: createUnit : unit not in file");
@@ -59,17 +54,16 @@ public class UnitManager {
 
 	public UnitMap getUnits() {
 
-		UnitMap uM;
-		IUnit iu;
+		UnitMap unitMap;
+		InterfaceUnit interfaceUnit;
 
-		uM = new UnitMap();
-		for (Element el : (List<Element>) XMLManager.getXML().getDocument()
-				.getRootElement().getChild("unitTable").getChildren("unit")) {
-			iu = new UnitProxy(el.getAttributeValue("uid"),
-					el.getAttributeValue("name"));
-			uM.put(iu.getUnitCode(), iu);
+		unitMap = new UnitMap();
+		for (Element element : (List<Element>) XMLManager.getXML().getDocument().getRootElement().getChild("unitTable")
+				.getChildren("unit")) {
+			interfaceUnit = new UnitProxy(element.getAttributeValue("unitId"), element.getAttributeValue("name"));
+			unitMap.put(interfaceUnit.getUnitCode(), interfaceUnit);
 		} // unit maps are filled with PROXY units
-		return uM;
+		return unitMap;
 	}
 
 }
